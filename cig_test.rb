4900bb0947ef7d542940baef0f5b45ebf771429a
misturfixit <japullins420@gmail.com>
2017-07-12 00:10:12 -0400

    aannd__dun

--- cig_test.rb
+++ cig_test.rb
@@ -7,18 +7,18 @@ require_relative "no_cig_1.rb"
 class TestArr < Minitest::Test
 
 
-	# def test_empty # no winning tix
-	# 	our_tikket = "3422"
-	# 	winning_tikket = []
-	# 	assert_equal([], find_winner(our_tikket, winning_tikket))
-	# end
+	def test_empty # no winning tix
+		our_tikket = "3422"
+		winning_tikket = []
+		assert_equal([], find_winner(our_tikket, winning_tikket))
+	end
 
 
                                                  #1234
 	def test_1_off       # off by one num ie.     #1224 
 	 	our_tikket = "3422"
-	 	winning_tikket = ["3421"]
-		assert_equal(true, offby_1(our_tikket, winning_tikket))
+	 	winning_tikkets = "3421"
+		assert_equal(true, offby_1(our_tikket, winning_tikkets))
 	end	
 
 
